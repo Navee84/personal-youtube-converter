f8c3c7331cf3571abd014488219ff26604199dab
unknown <xlarehc.games@gmail.com>
2025-02-10 01:01:38 +0100

    finished the queue frame + overall polish

--- ui_tests.py
+++ ui_tests.py
@@ -7,6 +7,7 @@ WINDOW_SIZE = "800x600"
 
 main_window.minsize(800,600)
 element_queue_font = tk.CTkFont(size=14)
+queue_header_font = tk.CTkFont(size=16,weight="bold")
 
 main_window.title("Youtube converter")
 main_window.geometry(WINDOW_SIZE)
@@ -20,6 +21,7 @@ main_window.grid_columnconfigure(2, weight=0)
 folder_icon = tk.CTkImage(dark_image=Image.open("assets/folder_light.png"),light_image=Image.open("assets/folder_dark.png"))
 confirm_icon = tk.CTkImage(dark_image=Image.open("assets/check_light.png"),light_image=Image.open("assets/check_dark.png"))
 trash_icon = tk.CTkImage(dark_image=Image.open("assets/bin_light.png"),light_image=Image.open("assets/bin_dark.png"))
+download_icon = tk.CTkImage(dark_image=Image.open("assets/download_light.png"),light_image=Image.open("assets/download_dark.png"))
 
 # QUEUE GLOBAL FRAME
 queue_global_frame =tk.CTkFrame(main_window)
@@ -30,13 +32,36 @@ queue_global_frame.grid_rowconfigure(0,weight=0)
 queue_global_frame.grid_rowconfigure(1,weight=1)
 
 # QUEUE HEADER FRAME
-queue_header_frame = tk.CTkFrame(queue_global_frame,fg_color="green",height=60)
+queue_header_frame = tk.CTkFrame(queue_global_frame,height=60)
 queue_header_frame.grid(row=0,column=0,sticky="ew")
 queue_header_frame.grid_rowconfigure(0,weight=1)
+for i in range(4):
+    queue_header_frame.grid_columnconfigure(i,weight=0)
+queue_header_frame.grid_columnconfigure(0,weight=3)
+queue_header_frame.grid_columnconfigure(1,weight=1)
+queue_header_frame.grid_columnconfigure(2,weight=1)
+queue_header_frame.grid_columnconfigure(3,weight=1)
+
+# header title
+header_title = tk.CTkLabel(queue_header_frame, fg_color="transparent", width=170, height=40,font=queue_header_font, text="Title")
+header_title.configure()
+header_title.grid(row=0, column=0, padx=(5,5), sticky="ew")
+
+# header length
+header_length = tk.CTkLabel(queue_header_frame, fg_color="transparent", width=60, height=40, font=queue_header_font, text="Length")
+header_length.grid(row=0, column=1, sticky="ew")
+
+# header size
+header_size = tk.CTkLabel(queue_header_frame, fg_color="transparent", width=60, height=40, font=queue_header_font, text="Size")
+header_size.grid(row=0, column=2, sticky="ew")
+
+# header format
+header_format = tk.CTkLabel(queue_header_frame, fg_color="transparent", width=60, height=40, font=queue_header_font, text="Format")
+header_format.grid(row=0, column=3, sticky="ew", padx=(0,110))
 
 
 # QUEUE FRAME
-queue_frame =tk.CTkScrollableFrame(queue_global_frame,fg_color="blue")
+queue_frame =tk.CTkScrollableFrame(queue_global_frame)
 queue_frame.grid(row=1, column=0, sticky="nsew")
 queue_frame.grid_columnconfigure(0,weight=1)
 
@@ -47,61 +72,51 @@ queue_frame.grid_rowconfigure(3,weight=1)
 
 
 # QUEUE INSTANCE TEST
-element_frame = tk.CTkFrame(queue_frame,fg_color="red",height=60)
+element_frame = tk.CTkFrame(queue_frame,height=60)
 element_frame.grid(row=0, column=0, sticky="ew",pady=3)
-element_frame.grid_rowconfigure(0,weight=1)
 for i in range(5):
     element_frame.grid_columnconfigure(i,weight=0)
+element_frame.grid_columnconfigure(0,weight=4)
+element_frame.grid_columnconfigure(1,weight=1)
+element_frame.grid_columnconfigure(2,weight=1)
+element_frame.grid_columnconfigure(3,weight=1)
+
+
 
 
 # element title
-element_title = tk.CTkTextbox(element_frame,fg_color="purple",font=element_queue_font, activate_scrollbars=True,height=80,width=170)
-element_title.insert("0.0","Example of a really fuckin long ass title like bro who tf has titles THAT long, get good at naming things fr")
-# element_title.insert("0.0","Hollow Knight OST - White Palace")
+element_title = tk.CTkTextbox(element_frame,font=element_queue_font, activate_scrollbars=True, height=80, width=180, fg_color="transparent")
+element_title.insert("0.0","Hollow Knight OST - White Palace")
+# element_title.insert("0.0","Example of a really fuckin long ass title like bro who tf has titles THAT long, get good at naming things fr")
 element_title.configure(state="disabled",wrap="word")
-element_title.grid(row=0, column=0, sticky="w", padx=5)
+element_title.grid(row=0, column=0, sticky="ew",padx=5,pady=5)
 
 # element length
-element_length = tk.CTkTextbox(element_frame,fg_color="purple",font=element_queue_font, activate_scrollbars=False,height=35,width=80)
+element_length = tk.CTkTextbox(element_frame,font=element_queue_font, activate_scrollbars=False, height=35, width=80)
 element_length.insert("0.0","0h2m15s")
 element_length.configure(state="disabled",wrap="none")
-element_length.grid(row=0, column=1, sticky="w", pady=20)
+element_length.grid(row=0, column=1, pady=20, sticky="ew")
 
 # element size
-element_length = tk.CTkTextbox(element_frame,fg_color="purple",font=element_queue_font, activate_scrollbars=False,height=35,width=60)
+element_length = tk.CTkTextbox(element_frame,font=element_queue_font, activate_scrollbars=False, height=35, width=60)
 element_length.insert("0.0","117Mo")
 element_length.configure(state="disabled",wrap="none")
-element_length.grid(row=0, column=2, sticky="w", pady=20)
+element_length.grid(row=0, column=2, pady=20, sticky="ew")
 
 # element format button
-element_format_button = tk.CTkSegmentedButton(element_frame,values=["MP3","MP4"],height=35,width=60,fg_color="purple")
+element_format_button = tk.CTkSegmentedButton(element_frame,values=["MP3","MP4"],height=35,width=60)
 element_format_button.set("MP3")
 element_format_button.configure(state="disabled")
-element_format_button.grid(row=0, column=3,padx=(10,0))
+element_format_button.grid(row=0, column=3,sticky="ew")
 
 # element directory button
 directory_button = tk.CTkButton(element_frame,image=folder_icon,text="",width=45,height=35)
-directory_button.grid(row=0,column=4)
+directory_button.grid(row=0,column=4, sticky="ew")
 
 # element remove button
 element_removre_button = tk.CTkButton(element_frame,image=trash_icon,text="",width=45,height=35)
-element_removre_button.grid(row=0,column=5)
+element_removre_button.grid(row=0,column=5, sticky="ew")
 
-
-
-
-
-# QUEUE INSTANCE TEST2
-element_frame = tk.CTkFrame(queue_frame,fg_color="red",height=95)
-element_frame.grid(row=1, column=0, sticky="ew",pady=3)
-
-# QUEUE INSTANCE TEST3
-element_frame = tk.CTkFrame(queue_frame,fg_color="red",height=95)
-element_frame.grid(row=2, column=0, sticky="ew",pady=3)
-
-# QUEUE INSTANCE TEST4
-element_frame = tk.CTkFrame(queue_frame,fg_color="red",height=95)
-element_frame.grid(row=3, column=0, sticky="ew",pady=3)
 
 
 
@@ -135,6 +150,8 @@ directory_button.grid(row=0,column=2,padx=10)
 confirm_button = tk.CTkButton(input_frame,image=confirm_icon,text="",width=50,height=35)
 confirm_button.grid(row=0,column=3)
 
+
+
 # SETTINGS FRAME
 settings_frame = tk.CTkFrame(main_window,fg_color="transparent")
 settings_frame.grid(row=1,column=2,sticky="nsew")
@@ -144,18 +161,16 @@ settings_frame.grid_columnconfigure(0,weight=1)
 for i in range(5):
     settings_frame.grid_rowconfigure(i, weight=1)
 
-# deflault folder button
-default_directory_button = tk.CTkButton(settings_frame,image=folder_icon,text="",width=70,height=35)
-default_directory_button.grid(row=0,column=0,padx=10)
+# download button
+download_button = tk.CTkButton(settings_frame, width=80, height=35, text="", image=download_icon)
+download_button.grid(row=0, column=0 ,padx=10)
 
-# default format button
-default_format_button = tk.CTkSegmentedButton(settings_frame,values=["MP3","MP4"],height=35)
-default_format_button.set("MP3")
-default_format_button.configure(state="disabled")
-default_format_button.grid(row=1, column=0, sticky="ew",pady=30,padx=10)
+# deflault folder button
+default_directory_button = tk.CTkButton(settings_frame,image=folder_icon,text="",width=80,height=35)
+default_directory_button.grid(row=1 ,column=0,padx=10)
 
 # empty queue button
-empty_queue = tk.CTkButton(settings_frame,image=trash_icon,text="",width=70,height=35)
+empty_queue = tk.CTkButton(settings_frame,image=trash_icon,text="",width=80,height=35)
 empty_queue.grid(row=2,column=0,padx=10)
 
 # queue size
